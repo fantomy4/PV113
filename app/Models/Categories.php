1db7eede19a00a48619098937435948d2ff3397e
Dmytro <ytfantom1@gmail.com>
2024-05-11 20:49:55 +0300

    homework

---
+++ app/Models/Categories.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @method static create(array $inputs)
+ */
+class Categories extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'image',
+        'is_delete'
+    ];
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+}
